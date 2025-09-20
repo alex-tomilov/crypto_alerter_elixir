085d60bedb560198812074669915dc84214b26ec
Alexey Tomilov <apofeosz@yandex.ru>
2025-09-20 20:56:33 +0500

    I-2: Try to fix the CI v1.5

--- lib/crypto_alerter_elixir_web/router.ex
+++ lib/crypto_alerter_elixir_web/router.ex
@@ -8,7 +8,7 @@ defmodule CryptoAlerterElixirWeb.Router do
     plug :fetch_live_flash
     plug :put_root_layout, html: {CryptoAlerterElixirWeb.Layouts, :root}
     plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug :put_secure_browser_headers, %{"content-security-policy" => "default-src 'self'"}
   end
 
   pipeline :api do
