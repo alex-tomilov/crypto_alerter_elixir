7817ec1f6d4450eebcf80f7a181057a02a7a9555
Alexey Tomilov <apofeosz@yandex.ru>
2025-09-26 21:37:46 +0500

    I-3: Added channel_configs table and ChannelConfig struct v1.0

---
+++ lib/crypto_alerter_elixir/channel_config.ex
@@ -0,0 +1,21 @@
+defmodule CryptoAlerterElixir.ChannelConfig do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "channel_configs" do
+    field :kind, Ecto.Enum, values: [email: 0, telegram: 1, webhook: 2, slack: 3], default: :email
+    field :name, :string
+    field :enabled, :boolean, default: false
+    field :secrets_ref, :string
+    field :rate_limit_per_min, :integer
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc false
+  def changeset(channel_config, attrs) do
+    channel_config
+    |> cast(attrs, [:kind, :name, :enabled, :secrets_ref, :rate_limit_per_min])
+    |> validate_required([:kind, :name, :enabled])
+  end
+end
