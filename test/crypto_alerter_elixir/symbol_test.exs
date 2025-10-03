b19dce19d8efd6b4770dde3dc310b7775f0660ad
Alexey Tomilov <apofeosz@yandex.ru>
2025-10-03 21:51:41 +0500

    I-4: Added validations' tests for the Symbol structure v1.0

---
+++ test/crypto_alerter_elixir/symbol_test.exs
@@ -0,0 +1,19 @@
+defmodule CryptoAlerterElixir.SymbolTest do
+  use CryptoAlerterElixir.DataCase, async: true
+  alias CryptoAlerterElixir.Symbol
+
+  test "name must be at least 3 characters long" do
+    changeset = Symbol.changeset(%Symbol{}, %{name: "AB"})
+    assert "should be at least 3 character(s)" in errors_on(changeset).name
+  end
+
+  test "name must be less than 255 characters long" do
+    changeset = Symbol.changeset(%Symbol{}, %{name: String.duplicate("A", 256)})
+    assert "should be at most 255 character(s)" in errors_on(changeset).name
+  end
+
+  test "changeset has no name errors if the name is valid" do
+    changeset = Symbol.changeset(%Symbol{}, %{name: "ABC"})
+    assert false == Enum.member?(changeset.errors, :name)
+  end
+end
