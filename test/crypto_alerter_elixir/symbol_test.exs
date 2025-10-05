e96f1f99e47d9daa25255e4ff90d3c14bef828cb
Alexey Tomilov <apofeosz@yandex.ru>
2025-10-05 21:39:44 +0500

    I-4: Added validations' tests for the Symbol structure v1.1

--- test/crypto_alerter_elixir/symbol_test.exs
+++ test/crypto_alerter_elixir/symbol_test.exs
@@ -1,6 +1,8 @@
 defmodule CryptoAlerterElixir.SymbolTest do
   use CryptoAlerterElixir.DataCase, async: true
-  alias CryptoAlerterElixir.Symbol
+  alias CryptoAlerterElixir.{Symbol, Repo}
+
+  @valid_attrs %{name: "ABC", provider: :binance, enabled: true}
 
   test "name must be at least 3 characters long" do
     changeset = Symbol.changeset(%Symbol{}, %{name: "AB"})
@@ -15,5 +17,12 @@ defmodule CryptoAlerterElixir.SymbolTest do
   test "changeset has no name errors if the name is valid" do
     changeset = Symbol.changeset(%Symbol{}, %{name: "ABC"})
     assert false == Enum.member?(changeset.errors, :name)
+  end
+
+  test "changeset is invalid if a name per a provide is used already" do
+    %Symbol{} |> Symbol.changeset(@valid_attrs) |> Repo.insert!()
+    another_symbol = %Symbol{} |> Symbol.changeset(@valid_attrs)
+    assert {:error, changeset} = Repo.insert(another_symbol)
+    assert "has already been taken" in errors_on(changeset).name
   end
 end
